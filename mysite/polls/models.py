fa7ca4ccbfe34f86b806d79fd9868086210b49d2
Nishant Garg <nisgarg86@gmail.com>
2022-07-23 17:12:05 +0530

    adding model definitions

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -7,8 +10,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('publish date')
 
+    def was_recently_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
