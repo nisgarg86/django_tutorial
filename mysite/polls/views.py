344fe4d6f34decf39155ff0b84b8ede457bf3286
Nishant Garg <nisgarg86@gmail.com>
2022-07-23 19:07:00 +0530

    adding view for question details

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Choice
 
@@ -7,12 +7,13 @@ from .models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context_dict = {'latest_question_list': latest_question_list}
-    template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render(context_dict, request))
+    return render(request, 'polls/index.html', context_dict)
 
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    question = get_object_or_404(Question, id=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
