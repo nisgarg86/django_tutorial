9a8f761e8304181ddc5f6b92b4c8350d6954a762
Nishant Garg <nisgarg86@gmail.com>
2022-07-23 18:37:18 +0530

    adding more views and template rendering

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse('Hello World. You are viewing polls index page')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context_dict = {'latest_question_list': latest_question_list}
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render(context_dict, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at results of question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting for question {question_id}')
